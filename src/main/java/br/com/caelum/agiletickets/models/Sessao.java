6d688fe0fa35d46700910f1328ebf061f27781c1
guilherme <guilherme.tamele@gmail.com>
2016-08-23 15:40:14 +0200

    corrigindo bug de numero de ingressos igual ao numero de vagas 2

--- src/main/java/br/com/caelum/agiletickets/models/Sessao.java
+++ src/main/java/br/com/caelum/agiletickets/models/Sessao.java
@@ -106,6 +106,7 @@ public class Sessao {
 		this.ingressosReservados += numeroDeIngressos;
 	}
 
+	
 	public boolean podeReservar(Integer numeroDeIngressos) {
 		int sobraram = getIngressosDisponiveis() - numeroDeIngressos;
         boolean naoTemEspaco = sobraram < 0;
